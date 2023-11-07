26abf94306727ef6736c77dfceecfdbf8754e61c
AlishaKang <oneoneone920920920@gmail.com>
2023-11-07 13:53:05 +0900

    Logout 기능 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm, CustomAuthenticationForm
 from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
 # Create your views here.
 def signup(request):
@@ -23,7 +24,7 @@ def login(request):
         if form.is_valid():
             # 로그인
             auth_login(request, form.get_user())
-            
+
             return redirect('articles:index')
     else:
         form = CustomAuthenticationForm()
@@ -32,3 +33,7 @@ def login(request):
         'form': form,
     }
     return render(request, 'login.html', context)
+
+def logout(request):
+    auth_logout(request)
+    return redirect('accounts:login')
